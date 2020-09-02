8bdae2443f5e097e90a8503e60e2380e0a2b0f5f
fukuda <2010d28.gifu@gmail.com>
2020-09-03 02:10:49 +0900

    はじめてのコミット

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const todos = [
+  {id: 0, title: 'Task 0', isDone: false},
+  {id: 1, title: 'Task 1', isDone: false},
+  {id: 2, title: 'Task 2', isDone: true}
+];
+
+function TodoItem(props) {
+  return (
+    <li key={props.todo.id}>
+      <label>
+      <input type="checkbox"
+        checked={props.todo.isDone}
+        />
+      {props.todo.title}
+      </label>
+    </li>
+  )
+}
+
+function TodoList(props) {
+  const todos = props.todos.map(todo => {
+    return (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+      />
+    );
+  });
+  
+  return (
+    <ul>
+      {todos}
+    </ul>
+  );
+}
+
+  class App extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        todos: todos
+      };
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>My Todo App </h1>
+          <TodoList todos={this.state.todos}/>
+        </div>
+      );
+    }
+  }
+  ReactDOM.render(
+      <App/>,
+      document.getElementById('root')
+      );
+
+
